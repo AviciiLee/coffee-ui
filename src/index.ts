76536bfa22b8283bd37e0773b9ee13ea19e538bc
AviciiLee <lx_ashin@aliyun.com>
2023-12-01 11:20:59 +0800

    转化为monorepo

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { randomNumber } from './utils'
-import './index.scss'
-console.log(randomNumber(1, 10))
--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export const randomNumber = (min: number, max: number) => {
-  min = Math.ceil(min)
-  max = Math.ceil(max)
-  return Math.floor(Math.random() * (max - min) + min)
-}
